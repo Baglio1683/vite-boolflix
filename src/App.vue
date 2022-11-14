5c0fde8cd83eb3cd8a6270ab248084a7656d6bcb
MyUsername <alberto.baglivi@gmail.com>
2022-11-14 12:12:08 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script>
+
+export default{
+
+components: {
+
+}, 
+
+data(){
+  return{
+    message : "hello"
+  }
+}
+
+
+
+
+
+}
+
+
+</script>
+
+<template>
+
+<div class="container"><h1>{{message}}</h1></div>
+
+</template>
+
+
+<style lang="scss">
+@use './styles/general.scss'; 
+
+
+h1{
+  color: red; 
+}
+
+</style>
