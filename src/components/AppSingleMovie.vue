f95b9669909bb66f8d1ce4eb2bd13b6af33b7370
MyUsername <alberto.baglivi@gmail.com>
2022-11-15 16:20:49 +0100

    Add cycle for vote

--- src/components/AppSingleMovie.vue
+++ src/components/AppSingleMovie.vue
@@ -6,7 +6,8 @@ export default{
 
      data(){
         return{
-         flags: ['en' , 'it']
+         flags: ['en' , 'it'], 
+         counter: [1]
         }
      },
 
@@ -27,6 +28,9 @@ export default{
 
          },
 
+        }, 
+    
+
     computed: {
 
         getTitle(){
@@ -37,22 +41,33 @@ export default{
             return this.movie.original_title ? this.movie.original_title : this.movie.original_name;
         }, 
         
+        getVote(){
 
-     }, 
-  
-   }
-  }
+             const i = parseInt(this.movie.vote_average)
+             console.log(i)
+
+             let newcount =[]
+             newcount.length = i
+             console.log(newcount)
+
+             this.counter = newcount
+             console.log(this.counter)
+
+             return 
+            },
+
+        }
+     }
 
 </script>
 
 <template>
 
-
     <h2>Titolo : {{getTitle}}</h2>
     <h3>Titolo originale : {{getOriginalTitle}}</h3>
     <img v-if="flags.includes(movie.original_language)" :src="getImgUrl(movie.original_language)" alt="">
     <p v-else>lingua originale : {{movie.original_language}}</p>
-    <p>Voto : {{movie.vote_average}}</p>
+    <span>Il voto è: {{getVote}} <i v-for="(item) in counter" class="fa-solid fa-star"></i></span>
     <img :src="getPosterImage(movie.poster_path)" alt="">
 
 </template>
